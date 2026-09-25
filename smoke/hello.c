0c76d41f33b608ac83c624f2d8d346cc241fa703
PedroMarioaBros <pedromariocaldas@gmail.com>
2026-09-25 12:20:30 -0300

    Initialize reproducible OpenXeChain X360 builder

---
+++ smoke/hello.c
@@ -0,0 +1,7 @@
+#include <stdint.h>
+volatile uint32_t pmcn_smoke_value = 0x58455832u;
+int main(void) {
+    pmcn_smoke_value ^= 0x360u;
+    for (;;) { __asm__ volatile("" ::: "memory"); }
+    return 0;
+}
